48dce6604c25f98d141528ba85534cc147ba2d9d
uen0_ <sarasa4869@zoho.com>
2021-11-04 15:38:58 +0900

    BGM追加、スプリクトの変更

--- Assets/Ueno/Sprict/BlockController.cs
+++ Assets/Ueno/Sprict/BlockController.cs
@@ -7,32 +7,41 @@ public class BlockController : MonoBehaviour
     [SerializeField] float _previousTime;
     // Blockが落ちる時間
     [SerializeField] float _fallTime = 1f;
+    //ブロックを動かしたときの音
+    [SerializeField]AudioSource _audio;
     //1ブロックの大きさ
     float _oneBlock = 1.5f;
     //動いて良いブロックかの判定
-    bool _fallBlock = true;
+    public bool _fallBlock = true;
+
     void Update()
     {
         if (_fallBlock == true)
-        BlockMove();
+        {
+            BlockMove();
+        }
     }
+
     private void BlockMove()
     {
         // 左矢印キーで左に動く
         if (Input.GetKeyDown(KeyCode.D))
         {
             transform.position += new Vector3(-_oneBlock, 0, 0);
+            _audio.Play();
         }
         // 右矢印キーで右に動く
         else if (Input.GetKeyDown(KeyCode.A))
         {
             transform.position += new Vector3(_oneBlock, 0, 0);
+            _audio.Play();
         }
         // クリックで４分の１回転
         else if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("マウスクリック");
             transform.Rotate(0, 0, 90);
+            _audio.Play();
         }
         // 自動で下に移動させつつ、下矢印キーでも移動する
         else if (Input.GetKeyDown(KeyCode.S) || Time.time - _previousTime >= _fallTime)
@@ -43,7 +52,7 @@ public class BlockController : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Block")
+        if (other.gameObject.tag == "End")
         {
             _fallBlock = false;
             Debug.Log("ブロックと接触した");
---
+++ Assets/Ueno/Sprict/OneBlockID.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneBlockID : MonoBehaviour
+{
+    private GameObject _block;
+    //ブロックの列のID
+    public int iD = -1;
+
+    //BlockController _fallBlock;
+
+    private void Start()
+    {
+        _block = GameObject.FindGameObjectWithTag("Block");
+        //_block = GameObject.Find("Block1");
+    }
+
+    void Update()
+    {
+        if (_block.GetComponent<BlockController>()._fallBlock == false)
+        {
+            this.gameObject.transform.parent = null;
+            this.tag = "End";
+        }
+    }
+}
