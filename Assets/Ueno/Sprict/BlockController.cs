481a02adcc6f609792ef925484096b2b466053a3
uen0_ <sarasa4869@zoho.com>
2021-11-04 13:42:10 +0900

    ブロックの操作スクリプトの追加

---
+++ Assets/Ueno/Sprict/BlockController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    [SerializeField] float _previousTime;
+    // Blockが落ちる時間
+    [SerializeField] float _fallTime = 1f;
+    //1ブロックの大きさ
+    float _oneBlock = 1.5f;
+    //動いて良いブロックかの判定
+    bool _fallBlock = true;
+    void Update()
+    {
+        if (_fallBlock == true)
+        BlockMove();
+    }
+    private void BlockMove()
+    {
+        // 左矢印キーで左に動く
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(-_oneBlock, 0, 0);
+        }
+        // 右矢印キーで右に動く
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(_oneBlock, 0, 0);
+        }
+        // クリックで４分の１回転
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("マウスクリック");
+            transform.Rotate(0, 0, 90);
+        }
+        // 自動で下に移動させつつ、下矢印キーでも移動する
+        else if (Input.GetKeyDown(KeyCode.S) || Time.time - _previousTime >= _fallTime)
+        {
+            transform.position += new Vector3(0, -_oneBlock, 0);
+            _previousTime = Time.time;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Block")
+        {
+            _fallBlock = false;
+            Debug.Log("ブロックと接触した");
+        }
+    }
+}
