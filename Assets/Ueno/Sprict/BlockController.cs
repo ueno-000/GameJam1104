56a65dd440fdc0fddbd977c272fdfc09e91c9aaa
uen0_ <sarasa4869@zoho.com>
2021-11-05 12:54:02 +0900

    Audio機能の削除

--- Assets/Ueno/Sprict/BlockController.cs
+++ Assets/Ueno/Sprict/BlockController.cs
@@ -8,7 +8,7 @@ public class BlockController : MonoBehaviour
     // Blockが落ちる時間
     [SerializeField] float _fallTime = 1f;
     //ブロックを動かしたときの音
-    [SerializeField]AudioSource _audio;
+   // [SerializeField]AudioSource _audio;
     //1ブロックの大きさ
     [SerializeField]float _oneBlock = 1.5f;
     //動いて良いブロックかの判定
@@ -28,20 +28,20 @@ public class BlockController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             transform.position += new Vector3(-_oneBlock, 0, 0);
-            _audio.Play();
+            //_audio.Play();
         }
         // 右矢印キーで右に動く
         else if (Input.GetKeyDown(KeyCode.D))
         {
             transform.position += new Vector3(_oneBlock, 0, 0);
-            _audio.Play();
+         //   _audio.Play();
         }
         // クリックで４分の１回転
         else if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("マウスクリック");
             transform.Rotate(0, 0, 90);
-            _audio.Play();
+           // _audio.Play();
         }
         // 自動で下に移動させつつ、下矢印キーでも移動する
         else if (Input.GetKeyDown(KeyCode.S) || Time.time - _previousTime >= _fallTime)
