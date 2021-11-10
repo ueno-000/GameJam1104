95558f581c3d283d3deaf86e8942bda569ea7a3a
HarashimaYoshikazu <harashima318@gmail.com>
2021-11-10 14:35:37 +0900

    個人用のシーンの追加、落下中のミノ制御スクリプトの追加（未完成）

---
+++ Assets/Harashima/Scripts/BlockStateController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockStateController : MonoBehaviour
+{
+    //生成後から着地するまでのミノの状態を管理するenum
+    enum BlockState
+    {
+        Start,
+        Fall,
+        Landing,
+        End
+    }
+    BlockState blockState;
+    void Start()
+    {
+        blockState = BlockState.Start;
+    }
+
+    //ミノの状態に応じて処理を行う関数
+    void CheckBlock()
+    {
+        while (blockState != BlockState.End)
+        {
+            switch (blockState)
+            {
+                case BlockState.Start:
+                    //ミノを生成する関数を呼びたい
+                    //ミノの種類を取得？
+                    blockState = BlockState.Fall;
+                    break;
+
+                case BlockState.Fall:
+                    //数秒おきに落下していく
+                    //落下中に移動キーが押されたら背景の色変化させる
+                    //落下移動
+                    if (Input.GetKeyDown(KeyCode.S))
+                    {
+                        
+                    }
+
+                    //左右移動
+                    if (Input.GetKeyDown(KeyCode.A))
+                    {
+
+                    }
+                    else if (Input.GetKeyDown(KeyCode.D))
+                    {
+
+                    }
+
+                    //回転ボタンが押された際に背景色の変更、下面の変更
+                    if (Input.GetButtonDown("Fire1"))
+                    {
+
+                    }
+                    //落下しきったことを判定してLandingへStateを変更する、下面の判定
+                    break;
+
+                case BlockState.Landing:
+                    //ミノがはみ出したらEndへ、それ以外はStartへ
+
+                    break;
+                    
+                case BlockState.End:
+                    break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
